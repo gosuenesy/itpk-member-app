aa7dcfdd7bc14230f3c7e83b49435c19fcfc5dd0
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2025-05-31 00:27:09 +0200

    reusable filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -22,58 +22,72 @@ const Filters = ({
   setBookingFilter,
 }) => (
   <Stack direction={{ xs: "column", sm: "row" }} spacing={2} sx={{ mb: 2 }}>
-    <TextField
-      label="Search Name"
-      variant="outlined"
-      size="small"
-      value={searchName}
-      onChange={(e) => setSearchName(e.target.value)}
-      fullWidth
-    />
-    <TextField
-      label="Search City"
-      variant="outlined"
-      size="small"
-      value={searchCity}
-      onChange={(e) => setSearchCity(e.target.value)}
-      fullWidth
-    />
-    <TextField
-      label="Search Email"
-      variant="outlined"
-      size="small"
-      value={searchEmail}
-      onChange={(e) => setSearchEmail(e.target.value)}
-      fullWidth
-    />
-    <FormControl size="small" fullWidth>
-      <InputLabel>Tag Filter</InputLabel>
-      <Select
-        value={selectedTag}
-        label="Tag Filter"
-        onChange={(e) => setSelectedTag(e.target.value)}
-      >
-        <MenuItem value="">All</MenuItem>
-        {tags.map((tag) => (
-          <MenuItem key={tag} value={tag}>
-            {tag}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
-    <FormControl size="small" fullWidth>
-      <InputLabel>Booking Filter</InputLabel>
-      <Select
-        value={bookingFilter}
-        label="Booking Filter"
-        onChange={(e) => setBookingFilter(e.target.value)}
-      >
-        <MenuItem value="all">All</MenuItem>
-        <MenuItem value="with">With Booking</MenuItem>
-        <MenuItem value="without">Without Booking</MenuItem>
-        <MenuItem value="only">Booking Without Conventus</MenuItem>
-      </Select>
-    </FormControl>
+    {searchName !== undefined && (
+      <TextField
+        label="Search Name"
+        variant="outlined"
+        size="small"
+        value={searchName}
+        onChange={(e) => setSearchName(e.target.value)}
+        fullWidth
+      />
+    )}
+
+    {searchCity !== undefined && (
+      <TextField
+        label="Search City"
+        variant="outlined"
+        size="small"
+        value={searchCity}
+        onChange={(e) => setSearchCity(e.target.value)}
+        fullWidth
+      />
+    )}
+
+    {searchEmail !== undefined && (
+      <TextField
+        label="Search Email"
+        variant="outlined"
+        size="small"
+        value={searchEmail}
+        onChange={(e) => setSearchEmail(e.target.value)}
+        fullWidth
+      />
+    )}
+
+    {selectedTag !== undefined && setSelectedTag && (
+      <FormControl size="small" fullWidth>
+        <InputLabel>Tag Filter</InputLabel>
+        <Select
+          value={selectedTag}
+          label="Tag Filter"
+          onChange={(e) => setSelectedTag(e.target.value)}
+        >
+          <MenuItem value="">All</MenuItem>
+          {tags?.map((tag) => (
+            <MenuItem key={tag} value={tag}>
+              {tag}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    )}
+
+    {bookingFilter !== undefined && setBookingFilter && (
+      <FormControl size="small" fullWidth>
+        <InputLabel>Booking Filter</InputLabel>
+        <Select
+          value={bookingFilter}
+          label="Booking Filter"
+          onChange={(e) => setBookingFilter(e.target.value)}
+        >
+          <MenuItem value="all">All</MenuItem>
+          <MenuItem value="with">With Booking</MenuItem>
+          <MenuItem value="without">Without Booking</MenuItem>
+          <MenuItem value="only">Booking Without Conventus</MenuItem>
+        </Select>
+      </FormControl>
+    )}
   </Stack>
 );
 
