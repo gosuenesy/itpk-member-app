3909b8a6b6350f34a50b7f01fc7b484a7ca56aae
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2025-05-30 23:41:14 +0200

    get bookings

--- src/proxy-server.mjs
+++ src/proxy-server.mjs
@@ -11,12 +11,13 @@ const PORT = process.env.PORT || 5000;
 
 const TOKEN_URL = "https://bookingportal.com/auth/token";
 const MEMBERS_URL =
-  "https://bookingportal.com/api/organizations/5d857c38-030c-47eb-826a-a74400a4fc12/memberslist";
+  "https://bookingportal.com/api/organizations/5d857c38-030c-47eb-826a-a74400a4fc12/memberslist?searchText=&offset=0&limit=10000";
 
 const CLIENT_CREDENTIALS = Buffer.from("3:thebookingportal").toString("base64");
 
 app.get("/api/members", async (req, res) => {
-  const conventusKey = "jHgcxKcct0egiOj7ghMy6dv0nTFwZnN0VUcivelOdpVm55jacVz8ZU8cECzuQdtC";
+  const conventusKey =
+    "jHgcxKcct0egiOj7ghMy6dv0nTFwZnN0VUcivelOdpVm55jacVz8ZU8cECzuQdtC";
   const conventusUrl = `https://www.conventus.dk/dataudv/api/adressebog/get_medlemmer.php?forening=16009&key=${conventusKey}&id=4388768&type=medlem`;
 
   try {
@@ -78,6 +79,46 @@ app.get("/api/booking-members", async (req, res) => {
     } else {
       res.status(500).json({ message: "Internal Server Error" });
     }
+  }
+});
+
+app.get("/api/bookings", async (req, res) => {
+  try {
+    const { from, days } = req.query;
+
+    const username = "api@itpk.dk";
+    const password = "ITPKAPI25";
+
+    const tokenResponse = await axios.post(
+      TOKEN_URL,
+      new URLSearchParams({
+        username,
+        password,
+        grant_type: "password",
+      }),
+      {
+        headers: {
+          Authorization: `Basic ${CLIENT_CREDENTIALS}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+    const BOOKINGS_URL = `https://bookingportal.com/api/offerings/0077d00a-e37a-49bf-b449-a74400a7f812/resourcebookings?from=${from}&days=${days}`;
+
+    const response = await axios.get(BOOKINGS_URL, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "User-Agent": "Node.js/Render",
+        Accept: "*/*",
+      },
+    });
+
+    res.json(response.data);
+  } catch (err) {
+    console.error("Booking data fetch failed:", err.message);
+    res.status(500).json({ error: err.message });
   }
 });
 
