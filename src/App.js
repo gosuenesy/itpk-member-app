0b24019195cd63d765352053741f67d433406dbf
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2025-05-30 23:40:06 +0200

    router + appbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
-import MemberList from "./components/MemberList";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  Container,
+} from "@mui/material";
+
+import MemberList from "./components/MemberList";
+import BookingsPage from "./components/BookingsPage";
+import { useFilteredMembers } from "./components/useFilteredMembers";
 
 const darkTheme = createTheme({
   palette: {
@@ -21,11 +39,64 @@ const darkTheme = createTheme({
   },
 });
 
+const Navigation = () => {
+  const location = useLocation();
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="lg">
+        <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            ITPK Admin
+          </Typography>
+          <Box>
+            <Button
+              component={Link}
+              to="/members"
+              color="inherit"
+              variant={location.pathname === "/members" ? "outlined" : "text"}
+              sx={{ mx: 1 }}
+            >
+              Members
+            </Button>
+            <Button
+              component={Link}
+              to="/bookings"
+              color="inherit"
+              variant={location.pathname === "/bookings" ? "outlined" : "text"}
+              sx={{ mx: 1 }}
+            >
+              Bookings
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
 function App() {
+  const { filtered, tags, setMembers } = useFilteredMembers({
+    selectedTag: "",
+    searchName: "",
+    searchCity: "",
+    searchEmail: "",
+    bookingFilter: "all",
+  });
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <MemberList />
+      <Router basename="/itpk-member-app">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<MemberList members={filtered} />} />
+          <Route path="/members" element={<MemberList members={filtered} />} />
+          <Route
+            path="/bookings"
+            element={<BookingsPage members={filtered} />}
+          />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
