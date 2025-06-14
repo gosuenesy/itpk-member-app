7e69edad08789580d45e90adb6d73d96537525a9
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2025-06-14 21:52:47 +0200

    full width filters

--- src/components/BookingsPage/BookingFilters.js
+++ src/components/BookingsPage/BookingFilters.js
@@ -5,7 +5,8 @@ import {
   MenuItem,
   Typography,
   Switch,
-  Box,
+  Stack,
+  InputLabel,
 } from "@mui/material";
 
 const BookingFilters = ({
@@ -20,9 +21,14 @@ const BookingFilters = ({
   sortDirection,
   setSortDirection,
 }) => (
-  <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap", mb: 2 }}>
-    <FormControl sx={{ minWidth: 140 }}>
-      <Select value={daysAgo} onChange={(e) => setDaysAgo(e.target.value)}>
+  <Stack direction={{ xs: "column", sm: "row" }} spacing={2} sx={{ mb: 2 }}>
+    <FormControl fullWidth size="small">
+      <InputLabel>From</InputLabel>
+      <Select
+        value={daysAgo}
+        onChange={(e) => setDaysAgo(e.target.value)}
+        label="From"
+      >
         {[0, 30, 60].map((val) => (
           <MenuItem key={val} value={val}>
             {val} days ago
@@ -30,28 +36,38 @@ const BookingFilters = ({
         ))}
       </Select>
     </FormControl>
-    <FormControl sx={{ minWidth: 140 }}>
-      <Select value={dayCount} onChange={(e) => setDayCount(e.target.value)}>
+
+    <FormControl fullWidth size="small">
+      <InputLabel>Next</InputLabel>
+      <Select
+        value={dayCount}
+        onChange={(e) => setDayCount(e.target.value)}
+        label="Next"
+      >
         {[7, 14, 30, 60].map((val) => (
           <MenuItem key={val} value={val}>
-            Next {val} days
+            {val} days
           </MenuItem>
         ))}
       </Select>
     </FormControl>
-    <FormControl sx={{ display: "flex", alignItems: "center" }}>
+
+    <FormControl fullWidth sx={{ flexDirection: "row", alignItems: "center" }}>
       <Typography variant="body2" sx={{ mr: 1 }}>
-        Only 1 Member
+        1 Member
       </Typography>
       <Switch
         checked={showOnlySingles}
         onChange={(e) => setShowOnlySingles(e.target.checked)}
       />
     </FormControl>
-    <FormControl sx={{ minWidth: 140 }}>
+
+    <FormControl fullWidth size="small">
+      <InputLabel>Sport</InputLabel>
       <Select
         value={sportFilter}
         onChange={(e) => setSportFilter(e.target.value)}
+        label="Sport"
         displayEmpty
       >
         <MenuItem value="all">All</MenuItem>
@@ -59,17 +75,20 @@ const BookingFilters = ({
         <MenuItem value="padel">Padel</MenuItem>
       </Select>
     </FormControl>
-    <FormControl sx={{ minWidth: 140 }}>
+
+    <FormControl fullWidth size="small">
+      <InputLabel>Sort</InputLabel>
       <Select
         value={sortDirection}
         onChange={(e) => setSortDirection(e.target.value)}
+        label="Sort"
         displayEmpty
       >
         <MenuItem value="asc">Oldest → Newest</MenuItem>
         <MenuItem value="desc">Newest → Oldest</MenuItem>
       </Select>
     </FormControl>
-  </Box>
+  </Stack>
 );
 
 export default BookingFilters;
